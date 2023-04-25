e18c47a1fbc39c02e8381845b2117492747c37aa
robertvitoriano <robertvitoriano@gmail.com>
2023-04-25 11:46:31 -0300

    add wall collisions with player

--- game.py
+++ game.py
@@ -8,21 +8,21 @@ HEIGHT = 600
 
 running = True
 speed = 0
-player_speed_rate = 3
-
+player_speed_rate = 8
+player_right_collision = False
+player_left_collision = False
 # create the screen
 screen = pygame.display.set_mode((WIDTH,HEIGHT))
 
 # Title and Icon
 pygame.display.set_caption("Space Invaders")
+background_image = pygame.image.load("background.png")
 
 # Player
 player_image = pygame.image.load('./player.png')
-
 player_x_coordinate = WIDTH/2 - player_image.get_width()
-
+player_x_position = player_x_coordinate
 player_y_position = HEIGHT - 100
-background_image = pygame.image.load("background.png")
 
 def draw_player(x, y):
     screen.blit(player_image,(x, y))
@@ -30,24 +30,40 @@ def draw_player(x, y):
 def render():
     screen.blit(background_image, (0, 0))
     handle_movements()
+    handle_player_wall_collisions()
     pygame.display.update()
 
 def handle_movements():
     keys = pygame.key.get_pressed()
-    player_x_position = get_player_x_position(keys)
+    handle_player_x_movements(keys)
     draw_player(player_x_position, player_y_position)
 
+def handle_player_wall_collisions():
+    global player_right_collision
+    global player_left_collision
+    global player_x_position
+    print(player_x_position)
+    if player_x_position >= WIDTH - player_image.get_width():
+        player_right_collision = True
+    else:
+        player_right_collision = False
 
-def get_player_x_position(keys):
+    if player_x_position <= 0:
+       player_left_collision = True
+    else:
+        player_left_collision = False
+
+def handle_player_x_movements(keys):
     global speed
+    global player_x_position
 
-    if keys[pygame.K_LEFT]:
+    if keys[pygame.K_LEFT] and not player_left_collision:
        speed-=player_speed_rate
 
-    if keys[pygame.K_RIGHT]:
+    if keys[pygame.K_RIGHT] and not player_right_collision:
        speed+=player_speed_rate
 
-    return player_x_coordinate + speed
+    player_x_position =  player_x_coordinate + speed
 
 while running:
 
